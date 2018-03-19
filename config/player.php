e107d44ac0f4481fbfad32d41b9c8fbeb2504def
Larry Morris <larry.morris@scientificgames.com>
2018-03-19 16:51:59 -0400

    removing .env.testing

--- config/player.php
+++ config/player.php
@@ -4,32 +4,16 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | Authentication Defaults
-    |--------------------------------------------------------------------------
+    | Player Required Fields
     |
-    | This option controls the default authentication "guard" and password
-    | reset options for your application. You may change these defaults
-    | as required, but they're a perfect start for most applications.
+    | These are the fields that MUST come in on every SPAT (X-Auth-SPAT) header in order
+    | to accept the player as valid. The registrar_id is always required, so no need
+    | to specify that. email and player_state are common fields you might require
     |
     */
 
-    "schema" => ["type" =>"group",
-                 "fields"=> [
-                     "color" => ["type"=>"text","value"=>"red"],
-                     "count" => ["type"=>"number","value"=>1,"valid"=>["min"=>1,"max"=>10]],
-                     "notify" => ["type"=>"group",
-                                  "fields"=> [
-                                      "playerid" => ["type"=>"text"],
-                                      "by" => ["type"=>"enum","valid"=>["email","push","messagecenter"],"value"=>"email"]
-                                  ] ],
-                     "games" => ["type"=>"multigroup", 
-                                 "extensible"=>true,
-                                 "fields"=> [
-                                     "name" => ["type"=>"text","valid"=>["sample"=>"Powerball"]],
-                                     "desc" => ["type"=>"text","valid"=>["sample"=>"A multi-state draw game"]],
-                                     "effective" => ["type"=>"text","valid"=>["regex"=>"\d{4}-\d{2}-\d{2}","sample"=>"YYYY-MM-DD"]]]
-                     ]
-                 ]
+    "required" => [
+        "email"
     ]
 
 ];
