22001b67f82b0391ec2d141ad8b107aae12cf4cd
Mark Cross <mark.cross@scientificgames.com>
2018-06-01 12:01:03 -0400

    php7.2/Laravel 5.6 ready to go

---
+++ app/Http/Controllers/CommonSwagger.php
@@ -0,0 +1,247 @@
+<?php
+
+// This file documents the calls provided by the eos-common package
+// so they appear on the Swagger-UI page. This file is published by
+// artisan vendor:publish from eos-common, and you should not edit it.
+/**
+* @SWG\Get(
+*   path="/api/schema",
+*   summary="REQUIRED API FOR SERVICES: Return settings pack schema",
+*   operationId="settingsSchema",
+*   tags={"eos"},
+* @SWG\Parameter(
+*     name="apikey",
+*     in="query",
+*     type="string",
+*     description="required api key"
+*  ),
+* @SWG\Response(response=200, description="successful",
+*     @SWG\Schema(
+*       type="string")
+*   ),
+* @SWG\Response(response=500, description="internal server error")
+* )
+*
+* @param Request $request
+* @return \Illuminate\Http\JsonResponse
+*/
+class apischemaget {}
+/**
+ * @SWG\Get(
+ *   path="/api/settings",
+ *   summary="REQUIRED API FOR SERVICES: Return settings",
+ *   operationId="getSettings",
+ *   tags={"eos"},
+ * @SWG\Parameter(
+ *     name="apikey",
+ *     in="query",
+ *     type="string",
+ *     description="required api key"
+ *  ),
+ * @SWG\Response(response=200, description="successful",
+ *     @SWG\Schema(
+ *       type="string")
+ *   ),
+ * @SWG\Response(response=500, description="internal server error")
+ * )
+ **/
+class apisettingsget {}
+/**
+* @SWG\Post(
+*   path="/api/settings",
+*   summary="REQUIRED API FOR SERVICES: Accept settings",
+*   operationId="postSettings",
+*   tags={"eos"},
+* @SWG\Parameter(
+*     name="apikey",
+*     in="query",
+*     type="string",
+*     description="required api key"
+*  ),
+* @SWG\Parameter(
+*   in="body",
+*   name="body",
+*   description="Settings JSON Data",
+*   required=true,
+*   @SWG\Schema(
+*       type="string")
+*   ),
+* @SWG\Response(response=200, description="successful",
+*     @SWG\Schema(
+*       type="string")
+*   ),
+* @SWG\Response(response=500, description="internal server error")
+* )
+*
+* @param Request $request
+* @return \Illuminate\Http\JsonResponse
+*/
+class apischemapost {}
+/**
+ * @SWG\Get(
+ *   path="/api/schedule",
+ *   summary="REQUIRED API FOR SERVICES: Return job schedule",
+ *   operationId="getSchedule",
+ *   tags={"eos"},
+ * @SWG\Parameter(
+ *     name="apikey",
+ *     in="query",
+ *     type="string",
+ *     description="required api key"
+ *  ),
+ * @SWG\Response(response=200, description="successful",
+ *     @SWG\Schema(
+ *       type="string")
+ *   ),
+ * @SWG\Response(response=500, description="internal server error")
+ * )
+ *
+ * @param Request $request
+ * @return \Illuminate\Http\JsonResponse
+ */
+class apischeduleget {}
+/**
+* @SWG\Delete(
+*   path="/api/settings",
+*   summary="REQUIRED API FOR SERVICES: Delete settings",
+*   operationId="deleteSettings",
+*   tags={"eos"},
+* @SWG\Parameter(
+*     name="apikey",
+*     in="query",
+*     type="string",
+*     description="required api key"
+*  ),
+* @SWG\Response(response=200, description="successful"),
+* @SWG\Response(response=500, description="internal server error")
+* )
+*
+* @param Request $request
+* @return \Illuminate\Http\JsonResponse
+*/
+class settingsdel {}
+/**
+* @SWG\Get(
+*     path="/api/oauth/clients",
+*     summary="GET OAUTH CLIENTS: only used by eos-mc.",
+*     operationId="getOauthClients",
+*     tags={"eos"},
+* @SWG\Parameter(
+*     name="apikey",
+*     in="query",
+*     type="string",
+*     description="required api key"
+*  ),
+*  @SWG\Response(response=200, description="successful",
+*      @SWG\Schema(
+*       type="array",
+*       @SWG\Items(ref="#/definitions/OauthClientResponse"))
+*   ),
+*   @SWG\Response(response=500, description="System error")
+* )
+*/
+class clientsget {}
+/**
+* @SWG\Post(
+*     path="/api/oauth/clients",
+*     summary="CREATE OAUTH CLIENT: only used by eos-mc.",
+*     operationId="createOauthClient",
+*     tags={"eos"},
+* @SWG\Parameter(
+*     name="apikey",
+*     in="query",
+*     type="string",
+*     description="required api key"
+*  ),
+* @SWG\Parameter(
+*     name="body",
+*     in="body",
+*     description="Client Data",
+*     required=true,
+*     @SWG\Schema(ref="#/definitions/OauthClientData")
+*   ),
+*   @SWG\Response(response=200, description="successful",
+*      @SWG\Schema(ref="#/definitions/OauthClientResponse")),
+*   @SWG\Response(response=500, description="System error")
+* )
+*/
+class clientspost {}
+/**
+* @SWG\Post(
+*   path="/oauth/token",
+*   summary="GET AN ACCESS TOKEN: You must know the client credentials id and secret.",
+*   operationId="getClientCredentialsToken",
+*   tags={"eos"},
+* @SWG\Parameter(
+*     name="body",
+*     in="body",
+*     description="Client Authorization",
+*     required=true,
+*     @SWG\Schema(ref="#/definitions/OauthGrantRequest")
+*   ),
+*   @SWG\Response(response=200, description="successful",
+*     @SWG\Schema(ref="#/definitions/OauthGrantResponse")),
+*   @SWG\Response(response=500, description="System error")
+*  )
+**/
+class tokenpost {}
+/**
+* strictly for Swagger doc
+* @SWG\Definition(required={"grant_type","client_id","client_secret","scope"}, type="object", @SWG\Xml(name="OauthGrantRequest"))
+* @SWG\Property(format="string", property="grant_type", example="client_credentials", description="Grant type")
+* @SWG\Property(type="string", property="client_id", example="2", description="ID of the credentials client")
+* @SWG\Property(type="string", property="client_secret", example="random-string", description="credentials client secret")
+* @SWG\Property(type="string", property="scope", example="manage-keys view-credentials", description="requested scopes")
+**/
+class OauthGrantRequest {}
+/**
+* strictly for Swagger doc
+* @SWG\Definition(required={"access_token","refresh_token","expires_in"}, type="object", @SWG\Xml(name="OauthGrantResponse"))
+* @SWG\Property(format="string", property="access_token", example="long-string", description="use as Bearer token")
+* @SWG\Property(type="string", property="refresh_token", example="long-string", description="use to refresh expired token")
+* @SWG\Property(type="number", property="expires_in", example="445632", description="seconds until expiration")
+**/
+class OauthGrantResponse {}
+
+/**
+* strictly for Swagger doc
+* @SWG\Definition(required={"id","name","secret","redirect",}, type="object", @SWG\Xml(name="OauthClientResponse"))
+* @SWG\Property(format="number", property="id", example="3", description="client Id")
+* @SWG\Property(format="string", property="name", example="eos-wallet", description="name of client")
+* @SWG\Property(type="string", property="secret", example="long-string", description="client Secret")
+* @SWG\Property(type="string", property="redirect", example="http:url", description="client redirect url")
+**/
+class OauthClientResponse {}
+/**
+* strictly for Swagger doc
+* @SWG\Definition(required={"name","redirect",}, type="object", @SWG\Xml(name="OauthClientData"))
+* @SWG\Property(format="string", property="name", example="eos-wallet", description="name of client")
+* @SWG\Property(type="string", property="redirect", example="http:url", description="client redirect url")
+**/
+class OauthClientData {}
+/**
+* strictly for Swagger doc
+* @SWG\Definition(required={"name","inbound","outbound"}, type="object", @SWG\Xml(name="ServiceConfig"))
+* @SWG\Property(format="string", property="name", example="Bonusing Manager", description="Name of the service")
+* @SWG\Property(type="array", property="inbound", description="All known connections to this service", @SWG\Items(ref="#/definitions/ConnectionConfig"))
+* @SWG\Property(type="array", property="outbound", description="All known connections from this service", @SWG\Items(ref="#/definitions/ConnectionConfig"))
+**/
+class ServiceConfig {}
+/**
+* strictly for Swagger doc
+* @SWG\Definition(required={"status"}, type="object", @SWG\Xml(name="ServiceConfigResponse"))
+* @SWG\Property(format="string", property="status", example="ok", description="Status return, ok or error")
+* @SWG\Property(type="string", property="config", example="{json string}", description="Summary of consumed configuration")
+* @SWG\Property(type="string", property="message", example="config failure", description="Any error message")
+**/
+class ServiceConfigResponse {}
+/**
+* strictly for Swagger doc
+* @SWG\Definition(required={"name","url"}, type="object", @SWG\Xml(name="ConnectionConfig"))
+* @SWG\Property(format="string", property="name", example="Bonusing Engine", description="Name of the service")
+* @SWG\Property(format="string", property="url", example="https://bonusing.pawtest.gamelogic.com", description="URL of the service.")
+* @SWG\Property(format="string", property="client_id", example=3, description="Oauth client ID.")
+* @SWG\Property(format="string", property="client_secret", example="34fcssec3w", description="Oauth client secret.")
+* @SWG\Property(format="string", property="client_callback", example="http://my.client/callback", description="Oauth client redirect url.")
+**/
+class ConnectionConfig {}
--- config/l5-swagger_old.php
+++ config/l5-swagger_old.php
@@ -1,17 +1,69 @@
 <?php
 
 return [
+
     'api' => [
+
         /*
         |--------------------------------------------------------------------------
         | Edit to set the api's title
         |--------------------------------------------------------------------------
         */
 
-        'title' => 'L5 Swagger UI',
+        'title' => 'Swagger UI',
+
+        /*
+        |--------------------------------------------------------------------------
+        | Edit to set the api's Auth token
+        |--------------------------------------------------------------------------
+        */
+
+        'auth_token' => env('L5_SWAGGER_API_AUTH_TOKEN', ""),
+
+        /*
+        |--------------------------------------------------------------------------
+        | Edit to set the api key variable in interface
+        |--------------------------------------------------------------------------
+        */
+
+        'key_var' => env('L5_SWAGGER_API_KEY_VAR', 'Authorization'),
+
+        /*
+        |--------------------------------------------------------------------------
+        | Edit to set the securityDefinition that is used in requests
+        |--------------------------------------------------------------------------
+        */
+
+        'security_definition' => env('L5_SWAGGER_API_SECURITY_DEFINITION', 'oauth2'),
+
+        /*
+        |--------------------------------------------------------------------------
+        | Edit to set where to inject api key (header, query)
+        |--------------------------------------------------------------------------
+        */
+
+        'key_inject' => env('L5_SWAGGER_API_KEY_INJECT', 'header'),
+
+        /*
+        |--------------------------------------------------------------------------
+        | Edit to set the api's version number
+        |--------------------------------------------------------------------------
+        */
+
+        'version' => env('L5_SWAGGER_API_VERSION', '1'),
+
+        /*
+        |--------------------------------------------------------------------------
+        | Edit to set the swagger version number
+        |--------------------------------------------------------------------------
+        */
+
+        'swagger_version' => env('L5_SWAGGER_DEFAULT_API_VERSION', '1'),
+
     ],
 
     'routes' => [
+
         /*
         |--------------------------------------------------------------------------
         | Route for accessing api documentation interface
@@ -30,26 +82,18 @@ return [
 
         /*
         |--------------------------------------------------------------------------
-        | Route for Oauth2 authentication callback.
-        |--------------------------------------------------------------------------
-        */
-
-        'oauth2_callback' => 'api/oauth2-callback',
-
-        /*
-        |--------------------------------------------------------------------------
         | Middleware allows to prevent unexpected access to API documentation
         |--------------------------------------------------------------------------
          */
         'middleware' => [
-            'api' => [],
-            'asset' => [],
+            'api' => ['web'],
             'docs' => [],
-            'oauth2_callback' => [],
         ],
+
     ],
 
     'paths' => [
+
         /*
         |--------------------------------------------------------------------------
         | Absolute path to location where parsed swagger annotations will be stored
@@ -76,6 +120,22 @@ return [
 
         /*
         |--------------------------------------------------------------------------
+        | Absolute path to directory where to export assets
+        |--------------------------------------------------------------------------
+        */
+
+        'assets' => public_path('vendor/l5-swagger'),
+
+        /*
+        |--------------------------------------------------------------------------
+        | Path to assets public directory
+        |--------------------------------------------------------------------------
+        */
+
+        'assets_public' => '/vendor/l5-swagger',
+
+        /*
+        |--------------------------------------------------------------------------
         | Absolute path to directory where to export views
         |--------------------------------------------------------------------------
         */
@@ -97,55 +157,7 @@ return [
         */
 
         'excludes' => [],
-    ],
 
-    /*
-    |--------------------------------------------------------------------------
-    | API security definitions. Will be generated into documentation file.
-    |--------------------------------------------------------------------------
-    */
-    'security' => [
-        /*
-        |--------------------------------------------------------------------------
-        | Examples of Security definitions
-        |--------------------------------------------------------------------------
-        */
-        /*
-        'api_key_security_example' => [ // Unique name of security
-            'type' => 'apiKey', // The type of the security scheme. Valid values are "basic", "apiKey" or "oauth2".
-            'description' => 'A short description for security scheme',
-            'name' => 'api_key', // The name of the header or query parameter to be used.
-            'in' => 'header', // The location of the API key. Valid values are "query" or "header".
-        ],
-        'oauth2_security_example' => [ // Unique name of security
-            'type' => 'oauth2', // The type of the security scheme. Valid values are "basic", "apiKey" or "oauth2".
-            'description' => 'A short description for oauth2 security scheme.',
-            'flow' => 'implicit', // The flow used by the OAuth2 security scheme. Valid values are "implicit", "password", "application" or "accessCode".
-            'authorizationUrl' => 'http://example.com/auth', // The authorization URL to be used for (implicit/accessCode)
-            //'tokenUrl' => 'http://example.com/auth' // The authorization URL to be used for (password/application/accessCode)
-            'scopes' => [
-                'read:projects' => 'read your projects',
-                'write:projects' => 'modify projects in your account',
-            ]
-        ],
-        */
-
-        /* Open API 3.0 support
-        'passport' => [ // Unique name of security
-            'type' => 'oauth2', // The type of the security scheme. Valid values are "basic", "apiKey" or "oauth2".
-            'description' => 'Laravel passport oauth2 security.',
-            'in' => 'header',
-            'scheme' => 'https',
-            'flows' => [
-                "password" => [
-                    "authorizationUrl" => config('app.url') . '/oauth/authorize',
-                    "tokenUrl" => config('app.url') . '/oauth/token',
-                    "refreshUrl" => config('app.url') . '/token/refresh',
-                    "scopes" => []
-                ],
-            ],
-        ],
-        */
     ],
 
     /*
@@ -174,31 +186,53 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | Configs plugin allows to fetch external configs instead of passing them to SwaggerUIBundle.
-    | See more at: https://github.com/swagger-api/swagger-ui#configs-plugin
+    | Edit to change layout of GUI ( 'none', 'list' or 'full')
     |--------------------------------------------------------------------------
     */
 
-    'additional_config_url' => null,
+    'docExpansion' => env('L5_SWAGGER_DOC_EXPANSION', 'none'),
 
     /*
     |--------------------------------------------------------------------------
-    | Apply a sort to the operation list of each API. It can be 'alpha' (sort by paths alphanumerically),
-    | 'method' (sort by HTTP method).
-    | Default is the order returned by the server unchanged.
+    | Edit to change the maximum number of characters to highlight code.
     |--------------------------------------------------------------------------
     */
-
-    'operations_sort' => env('L5_SWAGGER_OPERATIONS_SORT', null),
+    'highlightThreshold' => env('L5_SWAGGER_HIGHLIGHT_THRESHOLD', 5000),
 
     /*
     |--------------------------------------------------------------------------
     | Uncomment to pass the validatorUrl parameter to SwaggerUi init on the JS
-    | side.  A null value here disables validation.
+    | side.  A null value here disables validation.  A string will override
+    | the default url.  If not specified, behavior is default and validation
+    | is enabled.
     |--------------------------------------------------------------------------
     */
 
-    'validator_url' => null,
+    //'validatorUrl' => null,
+
+    'headers' => [
+
+        /*
+        |--------------------------------------------------------------------------
+        | Uncomment to add response headers when swagger is generated
+        |--------------------------------------------------------------------------
+        */
+
+        //'view' => [
+        //  'Content-Type' => 'text/plain',
+        //],
+
+        /*
+        |--------------------------------------------------------------------------
+        | Uncomment to add request headers when swagger performs requests
+        |--------------------------------------------------------------------------
+        */
+
+        //'request' => [
+        //  'TestMe' => 'testValue',
+        //],
+
+    ],
 
     /*
     |--------------------------------------------------------------------------
@@ -206,6 +240,7 @@ return [
     |--------------------------------------------------------------------------
      */
     'constants' => [
-        'L5_SWAGGER_CONST_HOST' => env('L5_SWAGGER_CONST_HOST', 'http://my-default-host.com'),
+        //'L5_SWAGGER_CONST_HOST' => env('L5_SWAGGER_CONST_HOST', 'http://my-default-host.com'),
     ],
+
 ];
